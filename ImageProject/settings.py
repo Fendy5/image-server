61e21f369be44f1d3c8842bf8f7609acd5301a21
Fendy <zf@fendy5.cn>
2023-08-10 15:33:37 +0800

    chore: 修复Database配置

--- ImageProject/settings.py
+++ ImageProject/settings.py
@@ -19,7 +19,6 @@ else:
     from .settings_dev import *
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -59,16 +58,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ImageProject.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
